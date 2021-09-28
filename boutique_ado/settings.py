f9c55fb4651b2d02e2fef45e14f22a530a3086c8
RvN-R <rvannairn@googlemail.com>
2021-09-28 15:39:35 +0000

    Bug Fix - spelling error in settings

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -184,7 +184,7 @@ if 'USE_AWS' in os.environ:
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
-    DEFAULT_FILE_STORAGE = 'custome_storages.MediaStorage'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
     # Override static and media URLs in production
